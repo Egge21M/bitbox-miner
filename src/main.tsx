80700a1f3b2a3effccc60f4b06fa4816611670d6
Egge <egge@getcurrent.io>
2024-09-16 12:17:23 +0200

    added background image

--- src/main.tsx
+++ src/main.tsx
@@ -22,7 +22,7 @@ function generateRandomHexStrings(count: number, length: number) {
 export const randomHexStrings = generateRandomHexStrings(100, 256);
 
 createRoot(document.getElementById("root")!).render(
-  <main className="absolute inset-0">
+  <main className="absolute inset-0" id="mainWrapper">
     <App />
   </main>,
 );
